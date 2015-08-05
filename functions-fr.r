31a51c390524b6c63d7b1a10c46352d073f7516e
François <briatte@gmail.com>
2015-08-05 21:35:54 +0200

    losing the geocodes

--- functions-fr.r
+++ functions-fr.r
@@ -41,56 +41,26 @@ clean_names <- function(x) {
   
 }
 
-#' Get French constituency geocodes
-#'
-#' Discards ex-colonies, overseas and parser bugs. The function is not run; the
-#' geocodes are directly provided in the repository instead.
-parse_geo <- function(x) {
-  
-  # drop locations outside of metropolitan France
-  x = x[ !grepl("hors de France|Somalis|Président|Orléansville-Médéa|Oran-Tlemcen|Oasis|Tizi-Ouzou|Alger|Constantine|Saoura|Guyane|Guadeloupe|La Réunion|Saint-Pierre-et-Miquelon|Martinique|Nouvelle-Calédonie|Polynésie|Wallis|Futuna|Saint-Barthélemy|Saint-Martin|Gabon|Tchad|Algérie|Oubangui|Affars", x) ]
-  x = na.omit(unique(x[ nchar(x) > 2 ]))
-  
-  # geocode constituency/canton
-  cat("Geocoding:", length(x), "constituencies\n")
-  cat("Google Maps API Terms of Service: http://developers.google.com/maps/terms\n")
-  
-  x = suppressMessages(cbind(constituency = x, 
-                             geocode(paste(x, "France"), output = "latlona")))
-  
-  # exclude imprecise results
-  return(subset(x, address != "france")[, -4])
-  
-}
-
 #' Get National Assembly legislature number
 #'
 #' Harmonizes network construction between both chambers. Discards legislation 
 #' examined by different sponsors during different legislatures.
 parse_legislature <- function(m, subset = 1:14) {
   l = rep(NA, length(m))
-  l [ m >= as.Date("1958-11-23") & m < as.Date("1962-11-18") ] = 1  # UNR
-  l [ m >= as.Date("1962-11-18") & m < as.Date("1967-03-05") ] = 2  # UNR
-  l [ m >= as.Date("1967-03-05") & m < as.Date("1968-06-23") ] = 3  # UD-Ve R
-  l [ m >= as.Date("1968-06-23") & m < as.Date("1973-03-04") ] = 4  # UDR
-  l [ m >= as.Date("1973-03-04") & m < as.Date("1978-03-12") ] = 5  # UDR
-  l [ m >= as.Date("1978-03-12") & m < as.Date("1981-06-14") ] = 6  # RPR
-  l [ m >= as.Date("1981-06-14") & m < as.Date("1986-03-16") ] = 7  # SOC
-  l [ m >= as.Date("1986-03-16") & m < as.Date("1988-06-05") ] = 8  # SOC/RPR split, PR, cohab.
-  l [ m >= as.Date("1988-06-05") & m < as.Date("1993-03-21") ] = 9  # SOC
-  l [ m >= as.Date("1993-03-21") & m < as.Date("1997-05-25") ] = 10 # RPR, cohab.
-  l [ m >= as.Date("1997-05-25") & m < as.Date("2002-06-09") ] = 11 # SOC, cohab.
-  l [ m >= as.Date("2002-06-09") & m < as.Date("2007-06-10") ] = 12 # UMP
-  l [ m >= as.Date("2007-06-10") & m < as.Date("2012-06-10") ] = 13 # UMP
-  l [ m >= as.Date("2012-06-10") & m < as.Date("2017-06-10") ] = 14 # SOC, ongoing
+  l[ m >= as.Date("1958-11-23") & m < as.Date("1962-11-18") ] = 1  # UNR
+  l[ m >= as.Date("1962-11-18") & m < as.Date("1967-03-05") ] = 2  # UNR
+  l[ m >= as.Date("1967-03-05") & m < as.Date("1968-06-23") ] = 3  # UD-Ve R
+  l[ m >= as.Date("1968-06-23") & m < as.Date("1973-03-04") ] = 4  # UDR
+  l[ m >= as.Date("1973-03-04") & m < as.Date("1978-03-12") ] = 5  # UDR
+  l[ m >= as.Date("1978-03-12") & m < as.Date("1981-06-14") ] = 6  # RPR
+  l[ m >= as.Date("1981-06-14") & m < as.Date("1986-03-16") ] = 7  # SOC
+  l[ m >= as.Date("1986-03-16") & m < as.Date("1988-06-05") ] = 8  # SOC/RPR split, PR, cohab.
+  l[ m >= as.Date("1988-06-05") & m < as.Date("1993-03-21") ] = 9  # SOC
+  l[ m >= as.Date("1993-03-21") & m < as.Date("1997-05-25") ] = 10 # RPR, cohab.
+  l[ m >= as.Date("1997-05-25") & m < as.Date("2002-06-09") ] = 11 # SOC, cohab.
+  l[ m >= as.Date("2002-06-09") & m < as.Date("2007-06-10") ] = 12 # UMP
+  l[ m >= as.Date("2007-06-10") & m < as.Date("2012-06-10") ] = 13 # UMP
+  l[ m >= as.Date("2012-06-10") & m < as.Date("2017-06-10") ] = 14 # SOC, ongoing
   l[ !l %in% subset ] = NA
   return(l)
-}
-
-#' Entropize latitude and longitude
-#' 
-#' Add some random noise to geographic coordinates to avoid overplotting
-entropize <- function(x, n) {
-  by = diff(range(x, na.rm = TRUE)) / 20
-  return(round(x + runif(n, min = by * -1, max = by), 2))
 }
